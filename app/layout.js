5093ab012796882f4f714df3eb858df6153d80b7
MantasEdine <youcefmantas3@gmail.com>
2026-02-09 13:54:33 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider dynamic>
+    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
